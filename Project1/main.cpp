9c31c637ba3c5c63bd0f8575edb79010dfe989e3
Danila <ddanilsachko@gmail.com>
2024-02-14 18:41:05 +0300

    Add project files.

---
+++ Project1/main.cpp
@@ -0,0 +1,49 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int aNum = 10;
+	int aNum2 = 20;
+	int aNum3 = 30;
+	float bNum;
+	long double cNum;
+	bool dNum;
+	string s;
+
+	cout << sizeof(aNum) << endl;
+	cout << sizeof(bNum) << endl;
+	cout << sizeof(cNum) << endl;
+	cout << sizeof(dNum) << endl;
+	cout << sizeof(s) << endl;
+
+	int* piNum = 0;
+	float* pfNum;
+	long double* pdNum;
+	bool* pbNum;
+	string* ps;
+
+	cout << "Addres of pointers: " << endl;
+	cout << sizeof(piNum) << endl;
+	cout << sizeof(pfNum) << endl;
+	cout << sizeof(pbNum) << endl;
+	cout << sizeof(pdNum) << endl;
+	cout << sizeof(ps) << endl;
+
+	cout << piNum << endl;
+	piNum = &aNum;
+	cout << piNum << endl;
+	cout << "Value of pointer = " << *piNum << endl;
+
+	*piNum *= 2;
+	cout << "Value of number = " << aNum << endl;
+
+	cout << "Address before sum : " << piNum << endl;
+	piNum += 1;
+	cout << "Address before sum : " << piNum << endl;
+
+	cout << "Address num2 "<< &aNum << endl;
+
+	return 0;
+}
